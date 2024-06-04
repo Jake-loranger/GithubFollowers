2e67db8bce4d79681337377a9998ae6bd8d3f5c0
Jake-loranger <106925875+Jake-loranger@users.noreply.github.com>
2024-06-04 16:24:55 -0400

    Initial commit

---
+++ GithubFollowers/Extensions/UIViewController+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Ext.swift
+//  GithubFollowers
+//
+//  Created by Jacob  Loranger on 6/3/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
---
+++ GithubFollowers/ViewControllers/FavoritesListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FavoritesListViewController.swift
+//  GithubFollowers
+//
+//  Created by Jacob  Loranger on 6/3/24.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
