92366fc9eb936987f51376204247979334f89168
Jake-loranger <106925875+Jake-loranger@users.noreply.github.com>
2024-06-04 20:00:13 -0400

    Followers List
    Implemented a Network call
    Created Models
    Created UICollectionView
    getData and map it to the UICollectionCells to put on UICollection View

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  GithubFollowers
+//
+//  Created by Jacob  Loranger on 6/4/24.
+//
+
+import Foundation
+
+struct Follower: Codable, Hashable {
+    var login: String
+    var avatarUrl: String
+    
+}
--- GithubFollowers/Support/SceneDelegate.swift
+++ GithubFollowers/Support/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
             guard let windowScene = (scene as? UIWindowScene) else { return }
         
@@ -105,7 +104,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
 
 }
 
