31bd8dfd6b626ad3c25a4429b7b525a3de59d79f
Jake-loranger <106925875+Jake-loranger@users.noreply.github.com>
2024-06-06 17:46:41 -0400

    User Header
    Implmented a childViewController for the user header that takes the users name, avatar, and location and populates the view when the view appears

---
+++ GithubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GithubFollowers
+//
+//  Created by Jacob  Loranger on 6/6/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
